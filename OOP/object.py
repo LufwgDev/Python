3611abdfd670432da350a213898e480ee5405fff
LufwgDev <lufwg@proton.me>
2025-02-11 14:04:25 -0500

    feat: :sparkles: hints, classFileds static and classmethod
    we use : str after the argument to indicate that the cosntructor can only get parameters that are string type. if a field is outside the constructor it can be acces from both, objects and class, but @staticmethod that can be executed also from both, ojects and class can NOT acces to class fields, on the other hand @classmethos its similar to @statics but  @classmetohds can access to class fields

--- OOP/object.py
+++ OOP/object.py
@@ -1,10 +1,43 @@
 class People:
-    def __init__(self, name):
+
+    # field that can be called from the classs
+    className = "People"
+
+    #Constructor
+    def __init__(self, name: str): #: str is a type hint that means string
+        #field that can only be called from the objects
         self.name = name
 
+    #Method
     def hi(self):
         print("Hi " +  self.name)
 
+    @staticmethod
+    # can NOT access to class fields
+    def helloWorld(): #def helloWorld(cls): 
+        print("Hello World")
+        # print("Hi" + cls.className) # error
+
+    @classmethod
+    #to access to class fields
+    def holaMundo(cls):
+        print("Hola Mundo")
+        print("Hi" + cls.className)
+
+#creating an instance of the method
 hector = People("Hector")
 
+#calling a method form the object
 hector.hi()
+
+#testing access to class fields from object and class
+print(hector.className)
+print(People.className)
+
+#testing access to class' static methods from object and class
+hector.helloWorld()
+People.helloWorld()
+#testing access to class' classmethods from object and class
+hector.holaMundo()
+People.holaMundo()
+
