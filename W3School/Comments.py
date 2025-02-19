c15a721062fc4658dfcabc7302d84bb1685da4a0
LufwgDev <lufwg@proton.me>
2025-02-19 14:00:36 -0500

    feat: :memo: comments

---
+++ W3School/Comments.py
@@ -0,0 +1,16 @@
+#this is a comment
+print("comments") #this is also a comment
+#print("Cheers, Mate!")
+
+#python does not
+#really have 
+# a syntax for
+#multiline comments
+
+"""
+Since pyton will ignore strings literals
+that are not assigned to a variable
+you can add multilingual sting (triple quotes)
+in your code and place your comment inside it
+"""
+print("comments again!")
