05cdfb8a7b0c77e237bc584ad1fd116c77007c42
LufwgDev <lufwg@proton.me>
2025-02-21 13:17:47 -0500

    feat: varNames
    how to name variables correctly

---
+++ W3School/variableNames.py
@@ -0,0 +1,18 @@
+#2myvar = "John"
+
+#  File "c:\Users\Estudiante\Desktop\Python\W3School\variableNames.py", line 1
+#    2myvar = "John"
+#    ^
+#SyntaxError: invalid decimal literal
+
+#my-var = "John"
+
+#    my-var = "John"
+#    ^^^^^^
+#SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+
+#my var = "John"
+
+#    my-var = "John"
+#    ^^^^^^
+#SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
