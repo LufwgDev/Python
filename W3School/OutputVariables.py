a453ca5a4af8e6e8aecb45e698443301442ebac9
LufwgDev <lufwg@proton.me>
2025-02-21 13:37:41 -0500

    feat: :sparkles: pritnting
    print fucntion

---
+++ W3School/OutputVariables.py
@@ -0,0 +1,17 @@
+x,y,z= "Python","is","awesome"
+
+print(x,y,z)
+print(y+x+z+"?")
+
+five= 5
+ten = 10
+print(five+ten)
+
+
+#print(five+x)
+
+#    print(five+x)
+#          ~~~~^~
+#TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+print(five,x)
