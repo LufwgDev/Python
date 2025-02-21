f54c3f7444c9b39d5c4c3575c8220d4866e6c2a8
LufwgDev <lufwg@proton.me>
2025-02-21 13:11:34 -0500

    feat: :sparkles: genaralities about variables
    casting, type, quotes, case

---
+++ W3School/Variables.py
@@ -0,0 +1,37 @@
+x= 5
+y= "Jhon"
+print(x)
+print(y)
+#weak
+x= "Sally"
+print(x)
+
+#casting
+x= str(3)
+y= int(3)
+z= float(3)
+print(x)
+print(y)
+print(z)
+
+#typeof
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Quotes
+y= 'even with casting'
+z= "it's still weakly typed"
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+caseSensitiveVariables = 2
+casesensitivevariables = "f"
+print(caseSensitiveVariables)
+print(casesensitivevariables)
