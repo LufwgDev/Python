43b0fe4e572ab34912789bf11193a596b9eea527
LufwgDev <lufwg@proton.me>
2025-02-27 14:55:52 -0500

    feat: :sparkles: Slicing strings ... to the end... with negative index... etc

---
+++ W3School/12.SlicingStrings.py
@@ -0,0 +1,21 @@
+b = "Hello, World!"
+
+print(b[1])
+
+#from 2 (included) to position 5 (not included)
+print(b[2:5])
+
+#from the start to position 5 (not included)
+print(b[:5])
+
+print(b[2:])
+
+#use Negative indexes to start from the end
+print(b[-5:-2])
+#from "o" in "World!" (positition -5)
+#to, but not included: "d" in "World!" (position -2)
+
+x=b[2:5]
+
+print(x)
+
