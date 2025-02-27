d2dada42a3c5f567c2ae773413689f143a443afa
LufwgDev <lufwg@proton.me>
2025-02-27 15:07:30 -0500

    feat: :sparkles: Upper, Lower,  strip, replace, split

---
+++ W3School/13.ModifyStrings.py
@@ -0,0 +1,15 @@
+a = "Hello, World!"
+print(a.upper())
+print(a)
+print(a.lower())
+
+b=" Hello World! "
+print(b)
+print(b.strip())
+
+print(a)
+print(a.replace("H","J"))
+
+print(a)
+print(a.split(",")) #['Hello', ' World!']
+
