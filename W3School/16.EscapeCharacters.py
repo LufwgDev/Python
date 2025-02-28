fa3cd2c0ae22d60868a6e2fa770bbcd4940c36d0
LufwgDev <lufwg@proton.me>
2025-02-28 12:48:26 -0500

    feat: Escape characters "\n"

---
+++ W3School/16.EscapeCharacters.py
@@ -0,0 +1,20 @@
+txt = "We are the so called \"Vikings\" from the nort"
+print(txt)
+single_quote = ' It\'s alright. '
+print(single_quote)
+backslash = " This will insert one \\ (backslash) "
+print(backslash)
+new_line = " Hello,\nWorld! "
+print(new_line)
+carriage_return= " Hello\rWorld! "
+print(carriage_return)
+tab = "Hello,\tWorld!"
+print(tab)
+backspace = "Hello, \bWorld!"
+print(backspace)
+from_feed = "Hello, \fWorld!"
+print(from_feed)
+octal_value = "\110\145\154\154\157 "
+print(octal_value)
+hex_value = "\x48\x65\x6c\x6c\x6f"
+print(hex_value)
